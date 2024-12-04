0b9f461f00ebc41148268d52eea848b3503b487c
Duc <cvducduy@gmail.com>
2024-12-04 10:39:55 +0700

    feat: get banners

--- controllers/banners.js
+++ controllers/banners.js
@@ -29,3 +29,20 @@ exports.updateBanner = async (req, res) => {
         return res.status(500).json({ status: false, message: 'Internal server error' });
     }
 }
+
+exports.getBanners = async (req,res) => {
+    try {
+        var banners = await Banner.find({})
+        if (banners.length > 0) {
+            const createdBanner = await Banner.create({
+                "banner": "https://static.vecteezy.com/system/resources/previews/010/566/757/non_2x/international-coffee-day-banner-design-with-wooden-background-illustration-vector.jpg",
+                "footer1": "https://static.vecteezy.com/system/resources/previews/012/025/024/non_2x/coffee-banner-ads-retro-brown-style-with-latte-and-coffee-beans-3d-realistic-simple-vector.jpg",
+                "footer2": "https://i.pinimg.com/736x/2e/d9/be/2ed9be4c75de693b75bacd6ed3594b31.jpg",
+            })
+            banners = [createdBanner]
+        }
+        return res.status(200).json({ status: true, message: 'Banners retrieved successfully', data: banners[0] });
+    } catch (error) {
+        
+    }
+}
--- routes/banners.js
+++ routes/banners.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router();
-const { updateBanner } = require('../controllers/banners');
+const { updateBanner, getBanners } = require('../controllers/banners');
 
 // Cập nhật banners
-// route: http://localhost:8080/banners
+// route: http://localhost:8080/banners/update
 // body: { banner, footer1, footer2 }
 router.put('/update', updateBanner);
+
+// Lấy banner
+// route: http://localhost:8080/banners/
+router.get('/', getBanners )
 
 module.exports = router;
